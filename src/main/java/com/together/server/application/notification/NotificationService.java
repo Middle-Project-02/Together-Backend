6481a627901d4d9f1b3db952aaeaf43c766b9b60
sihyuuun <leesh3597@gmail.com>
2025-06-24 00:45:55 +0900

    feat: 알림장 작성 시 푸시 알림 기능 추가 #45

--- src/main/java/com/together/server/application/notification/NotificationService.java
+++ src/main/java/com/together/server/application/notification/NotificationService.java
@@ -3,9 +3,12 @@ package com.together.server.application.notification;
 import com.together.server.application.notification.request.NotificationCreateRequest;
 import com.together.server.application.notification.response.NotificationDetailResponse;
 import com.together.server.application.notification.response.NotificationSimpleResponse;
+import com.together.server.domain.member.Member;
+import com.together.server.domain.member.MemberRepository;
 import com.together.server.domain.notification.Notification;
 import com.together.server.domain.notification.NotificationRepository;
 import com.together.server.domain.notification.validator.NotificationCreateValidator;
+import com.together.server.infra.web.FcmService;
 import com.together.server.support.error.CoreException;
 import com.together.server.support.error.ErrorType;
 import lombok.RequiredArgsConstructor;
@@ -19,14 +22,25 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class NotificationService {
 
+    private final MemberRepository memberRepository;
     private final NotificationRepository notificationRepository;
     private final NotificationCreateValidator notificationCreateValidator;
+    private final FcmService fcmService;
 
     @Transactional
     public void createNotification(NotificationCreateRequest request) {
         notificationCreateValidator.validate(request);
         Notification notification = new Notification(request.title(), request.summary(), request.issue(), request.impact(), request.solution(), request.tags());
         notificationRepository.save(notification);
+
+        List<Member> membersWithToken = memberRepository.findByFcmTokenIsNotNull();
+        for (Member member : membersWithToken) {
+            fcmService.sendNotification(
+                    member.getFcmToken(),
+                    request.title(),
+                    request.summary()
+            );
+        }
     }
 
     @Transactional(readOnly = true)
